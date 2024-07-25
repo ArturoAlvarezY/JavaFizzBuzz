a809df5bb912fc6e0e15a4c7934ebf3f4d77f869
ArturoAlvarezY <arturoavilamarketing@gmail.com>
2024-07-25 21:16:15 +0200

    logical way ready

---
+++ src/main/java/dev/arturo/models/FizzBuzz.java
@@ -0,0 +1,27 @@
+package dev.arturo.models;
+
+public class FizzBuzz {
+    
+    public String checkNumber(int numb){
+
+        boolean divisibleBy3 = numb % 3 == 0;
+        boolean containsThree = Integer.toString(numb).contains("3");
+
+        boolean divisibleBy5 = numb % 5 == 0;
+        boolean containsFive = Integer.toString(numb).contains("5");
+
+    
+    if(divisibleBy3 && divisibleBy5){
+        return "FizzBuzz";
+    }
+    else if (divisibleBy3 || containsThree) {
+        return "Fizz";
+    } 
+    else if(divisibleBy5 || containsFive){
+        return "Buzz";
+    }
+    else{
+        return Integer.toString(numb);
+    }
+    }
+}
